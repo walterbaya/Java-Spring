641a48028bfd5fad51a91f0bbd0c251d5b7e9bc0
walterbaya <walterbaya@yahoo.com>
2021-12-08 14:32:45 -0300

    se sube proyecto por linea de comando utilizando logger

---
+++ DemoConsola/src/main/java/com/mitocode/DemoConsolaApplication.java
@@ -0,0 +1,25 @@
+package com.mitocode;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConsolaApplication implements CommandLineRunner{
+
+	private static Logger LOG = LoggerFactory.getLogger(DemoConsolaApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConsolaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		//System.out.println("Hola mundo!");
+		LOG.info("Hola mundo optimizado!");
+		LOG.warn("Mensaje de advertencia");
+	}
+
+}
