19ccc23bece5ec3d1d00df6dc87707dc85b7037e
walterbaya <walterbaya@yahoo.com>
2021-12-26 20:57:02 -0300

    se descomprime el archivo

---
+++ Clase 2/student-crud/src/main/java/com/educacionit/StudentCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.educacionit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCrudApiApplication.class, args);
+	}
+
+}
